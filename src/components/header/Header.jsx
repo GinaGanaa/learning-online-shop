68cfb4d5386223c6433c63c5000290a29dc4645a
gina <otgoo.gantsetseg@gmail.com>
2023-11-16 01:58:03 -0500

    react project header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,23 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div>
+        <input type="text" id="search" name="Search" placeholder="Search" />
+        <button>Search</button>
+      </div>
+      <div>
+        <select name="Category" id="Category">
+          <option value="">Electronics</option>
+        </select>
+        <select name="Brand" id="Brand">
+          <option value="">Apple</option>
+        </select>
+        <select name="Sort" id="Sort">
+          <option value="">By Rating</option>
+          <option value="">By Price</option>
+        </select>
+      </div>
+    </div>
+  );
 }
 export default Header;
